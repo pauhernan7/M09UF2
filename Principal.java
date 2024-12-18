0df0d83d33cb3f4425a530a727a75d3c9ea95f49
pauhernan7 <146319055+pauhernan7@users.noreply.github.com>
2024-12-18 23:39:39 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,76 +1,62 @@
+// Principal.java
 public class Principal {
     public static void main(String[] args) {
-        // Comportament 1: Execució intercalada
         System.out.println("Comportament 1:");
         executarIntercalada();
 
-        // Comportament 2: Primer Pepe, després Juan
         System.out.println("\nComportament 2:");
         executarPrimerPepeDespresJuan();
 
-        // Comportament 3: Execució alterna estricta
         System.out.println("\nComportament 3:");
         executarAlternaEstricta();
     }
 
-    public static void executarIntercalada() {
-        Fil juan = new Fil("Juan", false);
-        Fil pepe = new Fil("Pepe", false);
+    private static void executarIntercalada() {
+        Thread juan = new Fil("Juan", false);
+        Thread pepe = new Fil("Pepe", false);
 
-        System.out.println("Termina thread main");
+        System.out.println("Main thread finalitzat.");
 
         juan.start();
         pepe.start();
 
-        try {
-            juan.join();
-            pepe.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        esperarFinalizacion(juan, pepe);
     }
 
-    public static void executarPrimerPepeDespresJuan() {
-        Fil pepe = new Fil("Pepe", false);
-        Fil juan = new Fil("Juan", false);
+    private static void executarPrimerPepeDespresJuan() {
+        Thread pepe = new Fil("Pepe", false);
+        Thread juan = new Fil("Juan", false);
 
-        System.out.println("Termina thread main");
+        System.out.println("Main thread finalitzat.");
 
-        // Donem prioritat a Pepe
-        pepe.setPriority(10);
-        juan.setPriority(1);
+        pepe.setPriority(Thread.MAX_PRIORITY);
+        juan.setPriority(Thread.MIN_PRIORITY);
 
         pepe.start();
         juan.start();
-        
-        try {
-            pepe.join();
-            juan.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-
+        esperarFinalizacion(pepe, juan);
     }
 
-    public static void executarAlternaEstricta() {
-        System.out.println("Termina thread main");
+    private static void executarAlternaEstricta() {
+        Thread juan = new Fil("Juan", true);
+        Thread pepe = new Fil("Pepe", true);
 
-        // Crear instancias de los hilos
-        Fil juan = new Fil("Juan", true);
-        Fil pepe = new Fil("Pepe",  true);
+        System.out.println("Main thread finalitzat.");
 
-        // Iniciar los hilos
         juan.start();
         pepe.start();
 
-        // Esperar a que ambos hilos terminen
-        try {
-            juan.join();
-            pepe.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        esperarFinalizacion(juan, pepe);
+    }
+
+    private static void esperarFinalizacion(Thread... threads) {
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Error esperant el fil: " + thread.getName());
+            }
         }
     }
 }
