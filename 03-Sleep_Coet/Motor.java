73c069ba5de9774e9a1f6bd5e98f1105483c9fc3
Pau Hernandez <2023_pau.hernandez@iticbcn.cat>
2025-01-15 16:00:35 +0100

    completada la part 2 coet

--- 03-Sleep_Coet/Motor.java
+++ 03-Sleep_Coet/Motor.java
@@ -19,24 +19,22 @@ public class Motor extends Thread {
                     }
                 }
 
-                // Si potenciaInicial es igual a potenciaObjectiu, el motor no hace nada
+                
                 if (potenciaObjectiu == 0 && potenciaInicial == 0) {
                     System.out.println("Motor apagat");
                     break;
                 }
 
-                // Comprobamos las tres condiciones y mostramos el mensaje correspondiente
                 if (potenciaInicial < potenciaObjectiu) {
-                    potenciaInicial++;  // Incrementamos la potencia
+                    potenciaInicial++;  
                     System.out.println(getName() + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
                 } else if (potenciaInicial > potenciaObjectiu) {
-                    potenciaInicial--;  // Decrementamos la potencia
+                    potenciaInicial--;  
                     System.out.println(getName() + ": Decre. Objetiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
                 } else {
                     System.out.println(getName() + ": FerRes. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
                 }
 
-                // Simulamos el ajuste con un retraso aleatorio
                 Random random = new Random();
                 Thread.sleep(1000 + random.nextInt(1000));
             }
