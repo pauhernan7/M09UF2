60a1fcef8377aa69f04cc2e76ce9fe40df0907dc
Pau Hernandez <2023_pau.hernandez@iticbcn.cat>
2025-01-16 18:12:52 +0100

    03-Sleep_i_Coet editat el codi

--- 03-Sleep_i_Coet/src/Motor.java
+++ 03-Sleep_i_Coet/src/Motor.java
@@ -3,44 +3,47 @@ import java.util.Random;
 public class Motor extends Thread {
     private int potenciaInicial = 0;
     private int potenciaObjectiu = 0;
+    private boolean enFuncionamiento = true; 
 
-    public synchronized void setPotenciaObjectiu(int potenciaObjectiu) {
+    public void setPotenciaObjectiu(int potenciaObjectiu) {
         this.potenciaObjectiu = potenciaObjectiu;
-        notify();
     }
 
     @Override
     public void run() {
-        try {
-            while (true) {
-                synchronized (this) {
-                    while (potenciaInicial == potenciaObjectiu) {
-                        wait();
-                    }
-                }
+        Random random = new Random();
 
-                
-                if (potenciaObjectiu == 0 && potenciaInicial == 0) {
-                    System.out.println("Motor apagat");
-                    break;
+        while (enFuncionamiento) {
+            if (potenciaInicial == potenciaObjectiu) {
+                try {
+                    Thread.sleep(100); 
+                } catch (InterruptedException e) {
+                    System.out.println(getName() + ": Interrumpido. Finalizando.");
+                    enFuncionamiento = false;
                 }
-
-                if (potenciaInicial < potenciaObjectiu) {
-                    potenciaInicial++;  
-                    System.out.println(getName() + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
-                } else if (potenciaInicial > potenciaObjectiu) {
-                    potenciaInicial--;  
-                    System.out.println(getName() + ": Decre. Objetiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
-                } else {
-                    System.out.println(getName() + ": FerRes. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
-                }
-
-                Random random = new Random();
-                Thread.sleep(1000 + random.nextInt(1000));
+                continue;
             }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            if (potenciaObjectiu == 0 && potenciaInicial == 0) {
+                System.out.println(getName() + ": Motor apagat");
+                enFuncionamiento = false;
+                break;
+            }
+
+            if (potenciaInicial < potenciaObjectiu) {
+                potenciaInicial++;
+                System.out.println(getName() + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
+            } else if (potenciaInicial > potenciaObjectiu) {
+                potenciaInicial--;
+                System.out.println(getName() + ": Decre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaInicial);
+            }
+
+            try {
+                Thread.sleep(1000 + random.nextInt(1000));
+            } catch (InterruptedException e) {
+                System.out.println(getName() + ": Interrumpido durante el ajuste. Finalizando.");
+                enFuncionamiento = false;
+            }
         }
     }
 }
